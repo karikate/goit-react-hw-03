d6751ecf51da43761ece9486418bd02dc0e7db4c
karikate <k.sarancha96@gmail.com>
2024-09-13 23:00:48 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchList from "./components/SearchList/SearchList";
+
+function App() {
+  return (
+    <>
+      <ContactForm />
+      <SearchList />
+      <ContactList />
+    </>
+  );
+}
+
+export default App;
