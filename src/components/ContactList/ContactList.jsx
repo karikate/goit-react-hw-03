59f7bc798d574bd8e2d8f24a712d1e4bcb5b8ce2
karikate <k.sarancha96@gmail.com>
2024-09-18 18:57:50 +0300

    for checking

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,20 @@
-const ContactList = () => {
-  return <div>ContactList</div>;
+import Contact from "../Contact/Contact";
+
+const ContactList = ({ contactData, handleDeleteContact }) => {
+  return (
+    <div>
+      <ul>
+        {contactData.map((contact) => (
+          <li key={contact.id}>
+            <Contact
+              contact={contact}
+              handleDeleteContact={handleDeleteContact}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ContactList;
--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,5 +1,17 @@
-const SearchList = () => {
-  return <div>SearchList</div>;
+import s from "./SearchList.module.css";
+const SearchList = ({ value, handleFilterContact }) => {
+  return (
+    <div>
+      <label className={s.label}>
+        <span>Find contacts by name</span>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => handleFilterContact(e.target.value)}
+        />
+      </label>
+    </div>
+  );
 };
 
 export default SearchList;
